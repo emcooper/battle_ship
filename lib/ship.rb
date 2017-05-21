276e8b107c3a0c7757638175f148743128d89939
Ellen Cooper <ellencooper74@gmail.com>
2017-05-21 16:48:07 -0600

    Ship sets coordinates; validate puts error for wrong # of coordinates

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,12 +1,20 @@
 class Ship
   attr_reader :size, :coordinates, :sunk
-  def initialize(size, coordinates)
+  def initialize(size)
     @size = size
-    @coordinates = coordinates
+    @coordinates = {}
     @sunk = false
   end 
   
+  def set_coordinates(coordinates_input)
+    #validated_input = vaidate(coordinates_input) if vaidate(coordinates_input)
+    coordinates_input.each do |input|
+      @coordinates[input] = " "
+    end 
+  end 
   
-  
-  
+  def validate(coordinates_input)
+    puts "Wrong number of coordinates!" if coordinates_input.length != @size 
+  end 
+
 end 
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -2,12 +2,28 @@ require './test/test_helper'
 require './lib/ship'
 
 class ShipTest < Minitest::Test 
-  def test_ship_initializes_with_size_coordinates_and_sunk_equals_false
-    ship = Ship.new(2, ["A1", "A2"])
+  def test_ship_initializes_with_size_blank_coordinates_and_sunk_equals_false
+    ship = Ship.new(2)
     
     assert_equal 2, ship.size
-    assert_equal ["A1", "A2"], ship.coordinates
+    assert_equal ({}), ship.coordinates
     assert_equal false, ship.sunk
+  end 
+  
+  def test_it_can_set_coordinates
+    ship = Ship.new(2)
+    ship.set_coordinates(["A1", "A2"])
+    
+    assert_equal ({"A1" => " ", "A2" => " "}), ship.coordinates
+  end 
+  
+  def test_validate_puts_error_for_wrong_number_of_coordinates
+    ship = Ship.new(2)
+    
+    expected = "Wrong number of coordinates!\n"
+    
+    assert_output(expected){ship.validate(["A1", "A2", "A2"])}
+    assert_output(expected){ship.validate(["A1"])}
   end 
   
   
