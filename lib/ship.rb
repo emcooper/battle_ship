20447bd2d0b9bcca8b6a044ee32e43a8ee1b7f95
Ellen Cooper <ellencooper74@gmail.com>
2017-05-21 22:24:09 -0600

    Ship set_coordinates works for 2-unit shup; row, column, direction, set_orientation methods added

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,20 +1,53 @@
 class Ship
-  attr_reader :size, :coordinates, :sunk
+  attr_reader :size, :coordinates, :orientation, :sunk
   def initialize(size)
     @size = size
     @coordinates = {}
+    @orientation = nil
     @sunk = false
   end 
   
-  def set_coordinates(coordinates_input)
+  def set_coordinates(first, last)
     #validated_input = vaidate(coordinates_input) if vaidate(coordinates_input)
-    coordinates_input.each do |input|
-      @coordinates[input] = " "
+    set_orientation(first, last)
+    @coordinates[first] = " "
+    @coordinates[last] = " "
+  end
+  
+  def set_orientation(first, last)
+    if row(first) == row(last)
+      @orientation = "horizontal"
+    elsif column(first) == column(last)
+      @orientation = "vertical"
     end 
   end 
   
+  def direction(first, last)
+      return "east" if column(first) < column(last)
+      return "west" if column(first) > column(last)
+      return "south" if row(first) < row(last)
+      return "north" if row(first) > row(last) 
+  end 
+
+  
+  def row(coordinate)
+    coordinate[0]
+  end 
+  
+  def column(coordinate)
+    coordinate[1].to_i
+  end 
+  
   def validate(coordinates_input)
-    puts "Wrong number of coordinates!" if coordinates_input.length != @size 
+    # todo need to know how Ship will interact with Board
+  end 
+  
+  def print_error(error_code)
+    errors = {1 => "You gave coordinates for the wrong ship size.",
+    2 => "Your ship cannot wrap around the board.",
+    3 => "Your ship cannot overlap with your other ships.",
+    4 => "Your ship must be horozontil or vertical."}
+    puts "#{errors[error_code]} Please try again: "
   end 
 
 end 
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -2,29 +2,76 @@ require './test/test_helper'
 require './lib/ship'
 
 class ShipTest < Minitest::Test 
-  def test_ship_initializes_with_size_blank_coordinates_and_sunk_equals_false
+  def test_ship_initializes_with_correct_values
     ship = Ship.new(2)
     
     assert_equal 2, ship.size
     assert_equal ({}), ship.coordinates
+    assert_nil ship.orientation
     assert_equal false, ship.sunk
   end 
   
-  def test_it_can_set_coordinates
+  def test_it_can_set_coordinates_of_2_unit_ship
     ship = Ship.new(2)
-    ship.set_coordinates(["A1", "A2"])
+    ship.set_coordinates("A1", "A2")
     
     assert_equal ({"A1" => " ", "A2" => " "}), ship.coordinates
   end 
   
-  def test_validate_puts_error_for_wrong_number_of_coordinates
+  def test_it_can_set_coordinates_of_3_unit_ship
+    skip 
+    ship = Ship.new(3)
+    ship.set_coordinates(["A1", "A3"])
+    expected = {"A1" => " ", "A2" => " ", "A3" => " " }
+    
+    assert_equal expected, ship.coordinates
+  end 
+  
+  def test_it_sets_horizontal_orientation
+    ship = Ship.new(3)
+    ship.set_orientation("A1", "A3")
+    
+    assert_equal "horizontal", ship.orientation
+  end 
+  
+  def test_it_sets_vertical_orientation
+    ship = Ship.new(3)
+    ship.set_orientation("C1", "A1")
+    
+    assert_equal "vertical", ship.orientation
+  end 
+  
+  def test_direction_returns_north_south_east_west
+    ship = Ship.new(3)
+    
+    assert_equal "north", ship.direction("C1", "A1")
+    assert_equal "south", ship.direction("B1", "D1")
+    assert_equal "east", ship.direction("C1", "C3")
+    assert_equal "west", ship.direction("A4", "A2")
+  end 
+  
+  def test_row_returns_row
+    ship = Ship.new(3)
+    
+    assert_equal "B", ship.row("B2")
+  end 
+  
+  def test_column_returns_column
+    ship = Ship.new(3)
+    
+    assert_equal 4, ship.column("C4")
+  end 
+  
+  def test_print_error_prints_error_based_on_error_code
     ship = Ship.new(2)
     
-    expected = "Wrong number of coordinates!\n"
+    expected_1 = "You gave coordinates for the wrong ship size. Please try again: \n"
+    expected_3 = "Your ship cannot overlap with your other ships. Please try again: \n"
     
-    assert_output(expected){ship.validate(["A1", "A2", "A2"])}
-    assert_output(expected){ship.validate(["A1"])}
+    assert_output(expected_1){ship.print_error(1)}
+    assert_output(expected_3){ship.print_error(3)}
   end 
+  
   
   
 end 
